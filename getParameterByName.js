dcc5a66e4400cbaf8083ea041bc2379a788b3c25
Steve Hong <smhong@gmail.com>
2012-04-22 12:00:23 -0400

    first commit

---
+++ getParameterByName.js
@@ -0,0 +1,11 @@
+//This useful function gets the request variables from a get request
+function getParameterByName(name) {
+	name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+	var regexS = "[\\?&]" + name + "=([^&#]*)";
+	var regex = new RegExp(regexS);
+	var results = regex.exec(window.location.search);
+	if(results == null)
+		return "";
+	else
+		return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
